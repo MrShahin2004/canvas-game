0e152c18882ca13aea1180f5d884e7e2885d9428
Mr Shahin <hashmyshahin@gmail.com>
2025-07-26 20:50:39 +0330

    First animation function :D

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const $ = document;
 
 const Canvas = $.querySelector("#canvas");
 let CTX = Canvas.getContext("2d");
+let CanvasWidth = Canvas.width = 600;
+let CanvasHeight = Canvas.height = 600;
+let x = 0;
+
+let PlayerImage = new Image();
+PlayerImage.src = "./assets/shadow_dog.png";
+
+function Animate() {
+    CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
+    CTX.fillRect(x, 50, 100, 100);
+    x++;
+
+    requestAnimationFrame(Animate);
+}
+
+Animate();
