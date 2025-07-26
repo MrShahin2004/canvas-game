712ad48ab26eafd7d12c062e30c959b9ac4b01c8
Mr Shahin <hashmyshahin@gmail.com>
2025-07-26 21:16:10 +0330

    Defining the values of width and height for a single frame.

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let CanvasHeight = Canvas.height = 600;
 let PlayerImage = new Image();
 PlayerImage.src = "./assets/shadow_dog.png";
 
+const SpriteWidth = 575;
+const SpriteHeight = 523;
+
 function Animate() {
     CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
     CTX.drawImage(PlayerImage, 0, 0, SpriteWidth, SpriteHeight, 0, 0, SpriteWidth, SpriteHeight);
