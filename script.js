a1820f95b592ebdb11f8749031e001f703769eb7
Mr Shahin <hashmyshahin@gmail.com>
2025-07-28 18:57:11 +0330

    Added the (x, y) position of each frame.

--- script.js
+++ script.js
@@ -17,7 +17,34 @@ let GameFrame = 0;
 let StaggerFrames = 5;
 
 let SpriteAnimations = [];
-let AnimationStates = [];
+let AnimationStates = [
+    {
+        name: "idle",
+        frames: 7
+    },
+    {
+        name: "jump",
+        frames: 7
+    }
+];
+
+AnimationStates.forEach((state,
+                         index) => {
+    let Frames = {
+        loc: []
+    };
+
+    for (let j = 0 ; j < state.frames ; j++) {
+        let PositionX = j * SpriteWidth;
+        let PositionY = index * SpriteHeight;
+
+        Frames.loc.push({x: PositionX, y: PositionY});
+    }
+
+    SpriteAnimations[state.name] = Frames;
+});
+
+console.log(SpriteAnimations);
 
 function Animate() {
     CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
