6e55230d90a9dbb8a33381f522cf8854a74b57e6
Mr Shahin <hashmyshahin@gmail.com>
2025-08-07 22:21:29 +0330

    And put it right here (to tell the browser run it when the page is loaded).

--- parallax.js
+++ parallax.js
@@ -18,12 +18,60 @@ BackgroundLayer4.src = "./assets/backgroundLayers/layer-4.png";
 let BackgroundLayer5 = new Image();
 BackgroundLayer5.src = "./assets/backgroundLayers/layer-5.png";
 
-let SliderEl = $.querySelector("#slider");
-SliderEl.value = GameSpeed;
-let ShowGameSpeed = $.querySelector("#show-game-speed");
-ShowGameSpeed.innerHTML = GameSpeed;
-
-SliderEl.addEventListener("change", (event) => {
-    GameSpeed = event.target.value;
+window.addEventListener("load", () => {
+    let SliderEl = $.querySelector("#slider");
+    SliderEl.value = GameSpeed;
+    let ShowGameSpeed = $.querySelector("#show-game-speed");
     ShowGameSpeed.innerHTML = GameSpeed;
+
+    SliderEl.addEventListener("change", (event) => {
+        GameSpeed = event.target.value;
+        ShowGameSpeed.innerHTML = GameSpeed;
+    });
+
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = GameSpeed * this.speedModifier;
+        }
+
+        update() {
+            this.speed = GameSpeed * this.speedModifier;
+            if (this.x <= -this.width) {
+                this.x = 0;
+            }
+
+            this.x = Math.floor(this.x - this.speed);
+        }
+
+        draw() {
+            CTX.drawImage(this.image, this.x, this.y, this.width, this.height);
+            CTX.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+
+    let Layer1 = new Layer(BackgroundLayer1, 0.25);
+    let Layer2 = new Layer(BackgroundLayer2, 0.5);
+    let Layer3 = new Layer(BackgroundLayer3, 0.75);
+    let Layer4 = new Layer(BackgroundLayer4, 1);
+    let Layer5 = new Layer(BackgroundLayer5, 1.25);
+
+    let GameObject = [Layer1, Layer2, Layer3, Layer4, Layer5];
+
+    function Animate() {
+        CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
+        GameObject.forEach((object) => {
+            object.draw();
+            object.update();
+        });
+
+        requestAnimationFrame(Animate);
+    }
+
+    Animate();
 });
