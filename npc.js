597ea494de76abf0d627a360b00179c3ec0fd710
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-11 21:23:16 +0330

    Refactored with class coding.

--- npc.js
+++ npc.js
@@ -6,15 +6,22 @@ let CTX = Canvas.getContext("2d");
 let CanvasWidth = Canvas.width = 500;
 let CanvasHeight = Canvas.height = 1000;
 
-// let Enemy1 = {
-//     x: 10,
-//     y: 50,
-//     width: 100,
-//     height: 100
-// };
+class Enemy {
+    constructor() {
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+}
+let Enemy1 = new Enemy();
 
 function Animate() {
     CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
+
+    Enemy1.x++;
+    Enemy1.y++;
+    CTX.fillRect(Enemy1.x, Enemy1.y, Enemy1.width, Enemy1.height);
 
     requestAnimationFrame(Animate);
 }
