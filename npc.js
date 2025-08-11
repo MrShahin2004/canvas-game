983b0f4ec054b07d94556ef5f8d695246f9716b4
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-11 20:31:14 +0330

    This was a test sample, we'll be creating them dynamically.

--- npc.js
+++ npc.js
@@ -6,12 +6,12 @@ let CTX = Canvas.getContext("2d");
 let CanvasWidth = Canvas.width = 500;
 let CanvasHeight = Canvas.height = 1000;
 
-let Enemy1 = {
-    x: 10,
-    y: 50,
-    width: 100,
-    height: 100
-};
+// let Enemy1 = {
+//     x: 10,
+//     y: 50,
+//     width: 100,
+//     height: 100
+// };
 
 function Animate() {
     CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
