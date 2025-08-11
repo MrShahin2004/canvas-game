f255e45f30237ae77e7f55dc59262a70e261b5f0
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-11 20:30:29 +0330

    A dummy square moving down slowly

--- npc.js
+++ npc.js
@@ -5,3 +5,22 @@ let Canvas = $.querySelector("#canvas");
 let CTX = Canvas.getContext("2d");
 let CanvasWidth = Canvas.width = 500;
 let CanvasHeight = Canvas.height = 1000;
+
+let Enemy1 = {
+    x: 10,
+    y: 50,
+    width: 100,
+    height: 100
+};
+
+function Animate() {
+    CTX.clearRect(0, 0, CanvasWidth, CanvasHeight);
+
+    Enemy1.x++;
+    Enemy1.y++;
+    CTX.fillRect(Enemy1.x, Enemy1.y, Enemy1.width, Enemy1.height);
+
+    requestAnimationFrame(Animate);
+}
+
+Animate();
