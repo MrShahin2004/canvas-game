9d5149e0d41ed12f428033e83a5990a18485ca17
MrShahin2004 <hashmyshahin@gmail.com>
2025-09-18 19:55:57 +0330

    Update npc.js
    Set to 0 for now.

--- npc.js
+++ npc.js
@@ -23,7 +23,7 @@ class Enemy {
         this.y = Math.random() * (CanvasHeight - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = Math.random() * 2;
+        this.angle = 0;
         this.angleSpeed = Math.random() * 0.2;
     }
 
