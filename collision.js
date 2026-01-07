93d33a6c4b40001caeb7183e8cc26770fe1cb097
MrShahin2004 <hashmyshahin@gmail.com>
2026-01-07 17:00:00 +0330

    Tested the code of collision of 2 rectangles.

--- collision.js
+++ collision.js
@@ -14,3 +14,15 @@ let Rectangle2 = {
     width: 10,
     height: 10
 };
+
+// Collision checking logic
+if (
+    Rectangle1.x < Rectangle2.x + Rectangle2.width &&
+    Rectangle1.x + Rectangle1.width > Rectangle2.width &&
+    Rectangle1.y < Rectangle2.y + Rectangle2.height &&
+    Rectangle1.y + Rectangle1.height > Rectangle2.height
+) {
+    console.log("Collision confirmed.");
+} else {
+    console.log("Collision not detected.");
+}
